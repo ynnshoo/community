d442f044e6dcd83cd40ae7534b597a895a9be278
ynns <zhaowuyan_5@163.com>
2021-03-09 22:08:26 +0800

    add mybatis

--- src/main/java/com/ynns/controller/AuthorizeController.java
+++ src/main/java/com/ynns/controller/AuthorizeController.java
@@ -2,12 +2,19 @@ package com.ynns.controller;
 
 import com.ynns.dto.AccessTokenDTO;
 import com.ynns.dto.GithubUser;
+import com.ynns.mapper.UserMapper;
+import com.ynns.pojo.User;
 import com.ynns.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -21,10 +28,14 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirect_uri;
 
+    @Autowired
+    UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
@@ -33,8 +44,25 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
 
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println("controller=====>"+user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println("controller=====>"+githubUser.getName());
+        if (githubUser!=null){
+            request.getSession().setAttribute("user",githubUser);
+            //登录成功 获取session、cookie
+            User user = new User();
+            user.setName(githubUser.getName());
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+//            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/ynns/controller/IndexController.java
+++ src/main/java/com/ynns/controller/IndexController.java
@@ -1,12 +1,32 @@
 package com.ynns.controller;
 
+import com.ynns.mapper.UserMapper;
+import com.ynns.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class IndexController {
+    @Autowired
+    UserMapper userMapper;
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
